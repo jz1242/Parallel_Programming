1b0b01fb328b600aadb525f5da1e6516ffe1cf0e
jz1242 <jz1242@gmail.com>
2018-04-01 12:00:54 -0400

    qwer

--- hw3/gameoflife.c
+++ hw3/gameoflife.c
@@ -8,11 +8,11 @@
 #include <string.h>
 #include "mpi.h"
 
-#define DEFAULT_ITERATIONS 64
+#define DEFAULT_ITERATIONS 52
 #define GRID_WIDTH  256
 #define DIM  16     // assume a square grid
 
-int checkNeighbors(int grid[], int ind) {
+int count(int grid[], int ind) {
     int col = ind % DIM;
     int row = ind / DIM;
     int totalNeighbors = 0;
@@ -40,7 +40,7 @@ int checkNeighbors(int grid[], int ind) {
 void update(int grid[], int begin, int end) {
     int nb[GRID_WIDTH];
     for (int i = begin; i < end; i++) {
-        nb[i] = checkNeighbors(grid, i);
+        nb[i] = count(grid, i);
     }
     for (int j = begin; j < end; j++) {
         if (grid[j] == 1){
@@ -57,11 +57,25 @@ void update(int grid[], int begin, int end) {
     }
 }
 
-int main ( int argc, char** argv ) {
-    
+void MPIcall(int global_grid[], int start, int end, int prev, int next, int temp[], int ID, MPI_Status stat, int block){
+    MPI_Gather(global_grid + start, block, MPI_INT, temp + start, block, MPI_INT, 0, MPI_COMM_WORLD);
+    if (ID % 2 == 0) {
+        MPI_Ssend(global_grid + start, DIM, MPI_INT, prev, 2, MPI_COMM_WORLD);
+        MPI_Recv(global_grid + (GRID_WIDTH + start-DIM)%GRID_WIDTH, DIM, MPI_INT, prev, 2, MPI_COMM_WORLD, &stat);
+        MPI_Ssend(global_grid + end - DIM, DIM, MPI_INT, next, 2, MPI_COMM_WORLD);
+        MPI_Recv(global_grid + end, DIM, MPI_INT, next, 2, MPI_COMM_WORLD, &stat);
+    } else {
+        MPI_Recv(global_grid + (GRID_WIDTH + end)%GRID_WIDTH, DIM, MPI_INT, next, 2, MPI_COMM_WORLD, &stat);
+        MPI_Ssend(global_grid + end-DIM, DIM, MPI_INT, next, 2, MPI_COMM_WORLD);
+        MPI_Recv(global_grid + (GRID_WIDTH + start-DIM)%GRID_WIDTH, DIM, MPI_INT, prev, 2, MPI_COMM_WORLD, &stat);
+        MPI_Ssend(global_grid + start, DIM, MPI_INT, prev, 2, MPI_COMM_WORLD);
+    }
 
-  int global_grid[ 256 ] = 
-   {0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+}
+int main ( int argc, char** argv ) {
+
+    int global_grid[ 256 ] = 
+    {0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
     0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,
     1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
@@ -78,75 +92,65 @@ int main ( int argc, char** argv ) {
     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
     0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
 
-  // MPI Standard variable
-  int num_procs;
-  int ID, j;
-  int iters = 0;
-  int num_iterations;
+    // MPI Standard variable
+    int num_procs;
+    int ID, j;
+    int iters = 0;
+    int num_iterations;
 
-  // Setup number of iterations
-  if (argc == 1) {
+    // Setup number of iterations
+    if (argc == 1) {
     num_iterations = DEFAULT_ITERATIONS;
-  }
-  else if (argc == 2) {
+    }
+    else if (argc == 2) {
     num_iterations = atoi(argv[1]);
-  }
-  else {
+    }
+    else {
     printf("Usage: ./gameoflife <num_iterations>\n");
     exit(1);
-  }
+    }
 
-  // Messaging variables
-  MPI_Status stat;
-  // TODO add other variables as necessary
+    // Messaging variables
+    MPI_Status stat;
+    // TODO add other variables as necessary
 
-  // MPI Setup
-  if ( MPI_Init( &argc, &argv ) != MPI_SUCCESS )
-  {
+    // MPI Setup
+    if ( MPI_Init( &argc, &argv ) != MPI_SUCCESS )
+    {
     printf ( "MPI_Init error\n" );
-  }
+    }
 
-  MPI_Comm_size ( MPI_COMM_WORLD, &num_procs ); // Set the num_procs
-  MPI_Comm_rank ( MPI_COMM_WORLD, &ID );
+    MPI_Comm_size ( MPI_COMM_WORLD, &num_procs ); // Set the num_procs
+    MPI_Comm_rank ( MPI_COMM_WORLD, &ID );
 
-  assert ( DIM % num_procs == 0 );
+    assert ( DIM % num_procs == 0 );
 
   // TODO Setup your environment as necessary
+    int block = DIM*(DIM/num_procs);
+    int start = block*ID;
+    int end = start + block;
+    int prev = (ID - 1) % num_procs < 0 ? ((ID - 1) % num_procs) + num_procs : (ID - 1) % num_procs;
+    int next = (ID + 1) % num_procs < 0 ? ((ID + 1) % num_procs) + num_procs : (ID + 1) % num_procs; 
+    int* temp = malloc(sizeof(int) * GRID_WIDTH * num_procs);
 
-  int block = DIM*(DIM/num_procs);
-  int start = block*ID;
-  int end = start + block;
-  int prev = (ID - 1) % num_procs < 0 ? ((ID - 1) % num_procs) + num_procs : (ID - 1) % num_procs;
-  int next = (ID + 1) % num_procs < 0 ? ((ID + 1) % num_procs) + num_procs : (ID + 1) % num_procs; 
-
-  for ( iters = 0; iters < num_iterations; iters++ ) {
-    // TODO: Add Code here or a function call to you MPI code
-    if (ID % 2 == 0) {
-      MPI_Ssend(global_grid + start, DIM, MPI_INT, prev, 2, MPI_COMM_WORLD);
-      MPI_Ssend(global_grid + end - DIM, DIM, MPI_INT, next, 2, MPI_COMM_WORLD);
-      MPI_Recv(global_grid + (GRID_WIDTH + start-DIM)%GRID_WIDTH, DIM, MPI_INT, prev, 2, MPI_COMM_WORLD, &stat);
-      MPI_Recv(global_grid + end, DIM, MPI_INT, next, 2, MPI_COMM_WORLD, &stat);
-    } else {
-      MPI_Recv(global_grid + (GRID_WIDTH + end)%GRID_WIDTH, DIM, MPI_INT, next, 2, MPI_COMM_WORLD, &stat);
-      MPI_Recv(global_grid + (GRID_WIDTH + start-DIM)%GRID_WIDTH, DIM, MPI_INT, prev, 2, MPI_COMM_WORLD, &stat);
-      MPI_Ssend(global_grid + end-DIM, DIM, MPI_INT, next, 2, MPI_COMM_WORLD);
-      MPI_Ssend(global_grid + start, DIM, MPI_INT, prev, 2, MPI_COMM_WORLD);
-    }
-    update(global_grid, start, end);
-    // Output the updated grid state
-    if ( ID == 0 ) {
-      printf ( "\nIteration %d: final grid:\n", iters );
-      for ( j = 0; j < GRID_WIDTH; j++ ) {
-        if ( j % DIM == 0 ) {
-          printf( "\n" );
+    for ( iters = 0; iters < num_iterations; iters++ ) {
+        update(global_grid, start, end);
+        MPIcall(global_grid, start, end, prev, next, temp, ID, stat, block);
+        
+        // Output the updated grid state
+        if ( ID == 0 ) {
+            printf ( "\nIteration %d: final grid:\n", iters );
+            for ( j = 0; j < GRID_WIDTH; j++ ) {
+                if ( j % DIM == 0 ) {
+                    printf( "\n" );
+                }
+                printf ( "%d  ", temp[j] );
+            }
+            printf( "\n" );
         }
-        printf ( "%d  ", global_grid[j] );
-      }
-      printf( "\n" );
     }
-  }
-
   // TODO: Clean up memory
+  free(temp);
   MPI_Finalize(); // finalize so I can exit
 }
 
